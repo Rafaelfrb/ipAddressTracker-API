31627cf167765a2d6a8a5f636bd7e23c6a11c2ac
Rafaelfrb <rafael.frb@hotmail.com>
2022-06-05 19:43:46 -0300

    Continuing working on script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_mzCEinScHa7QTAjGtsWNZbO27JBNi&ipAddress=${ip}`
 
 const ip = document.querySelector(".inputip");
+
+function (ip) {
+    fetch('url')
+      .then(function (response)) {
+        console.log(response)
+    }
+}
