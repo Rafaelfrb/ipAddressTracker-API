7d822461ecef98859c108e8787be6bacc3b1452b
Rafaelfrb <rafael.frb@hotmail.com>
2022-06-10 10:32:34 -0300

    Cliente IP automatically appears in the search box now

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 
+	fetch('https://api.ipify.org/?format=json')
+	.then(results => results.json())
+	.then(data => {
+		console.log(data.ip);
+    document.getElementById('inputip').value = data.ip;
+	})
+
+getData();
+
 async function getData() {
+
   let ip = document.getElementById('inputip').value;
+
   let url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_mzCEinScHa7QTAjGtsWNZbO27JBNi&ipAddress=${ip}`
+  console.log(url);
   const response = await fetch(url);
 
   const data = await response.json();
@@ -47,5 +59,3 @@ async function getData() {
   let container = document.querySelector('.container');
   container.innerHTML = html;
 };
-
-//getData();
